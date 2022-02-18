6d30fd9535a89a8973e400ed7201d1eec14f62cc
TheKennyman07 <69918528+KennyMan14@users.noreply.github.com>
2022-02-17 20:47:36 -0600

    UPDATE the main files

--- crimsonrealm/game.js
+++ crimsonrealm/game.js
@@ -1,12 +1,62 @@
+function fakeBody(){
+  return {
+    id: 'fakeBody',
+    require: ['area', 'pos', 'body'],
+    add(){
+      //nothing :)
+    },
+    update(){
+      if(this.c('body')){
+        if(this.isGrounded()){
+          this.unuse('body');
+        }
+      }else {
+        this.unuse('fakebody');
+      }
+    }
+  }
+}
+function patrol(type){
+  const limit = {min: Number.NEGATIVE_INFINITY, max: Number.POSITIVE_INFINITY};
+  const speed = 60;
+  let dir = -1;
+  return {
+    id: 'patrol',
+    require: ['area', 'pos', 'sprite'],
+    add(){
+      if(type == 'skeleton'){
+        limit.min = this.pos.x - 100;
+        limit.max = this.pos.x + 100
+      } 
+    },
+    update(){
+      if(type == 'skeleton'){
+        if(this.pos.x < limit.min){
+          dir = 1;
+          this.flipX(true);
+        }
+        if(this.pos.x > limit.max){
+          dir = -1;
+          this.flipX(false);
+        }
+        this.move(speed*dir, 0);
+      }
+    }
+  }
+}
+
+const SPEED = 130;
+const DISPLACEMENT = 160;
+
 scene('play', () => {
   camScale(2);
 
   const map = addLevel([
     "            ",
     "            ",
-    "            ",
-    "==== =======",
-    "            ",
+    "            s     ",
+    "==== ==============",
+    "|||| ||||||||||||||",
     "            ",
   ], {
     width: 32,
@@ -16,7 +66,22 @@ scene('play', () => {
       sprite('tiles', {frame: 0}),
       area(),
       solid(),
-    ]
+    ],
+    "|": () => [
+      rect(32, 41),
+      color(0, 0, 32),
+      area(),
+      solid(),
+    ],
+    "s": () => [
+      sprite('skeleton', {anim: 'walk'}),
+      body(),
+      fakeBody(),
+      area({scale: 0.6, offset: vec2(10, 10)}),
+      solid(),
+      patrol('skeleton'),
+      "enemy"
+    ],
   })
 
   const hero = add([
@@ -33,6 +98,18 @@ scene('play', () => {
       jumping: false,
       crouching: false,
       hurt: false,
+      dir: 1,
+    }
+  ])
+  const swordCollider = add([
+    rect(30, 10),
+    area(),
+    pos(),
+    opacity(0),
+    follow(hero, vec2(20, -30)),
+    "sword",
+    {
+      attacking: false,
     }
   ])
 
@@ -41,14 +118,16 @@ scene('play', () => {
   });
   onKeyDown('left', () => {
     hero.flipX(true);
+    hero.dir = -1;
     if(!hero.attacking && !hero.hurt && !hero.crouching){
-      hero.move(-130, 0)
+      hero.move(-SPEED, 0)
     }
   })
   onKeyDown('right', () => {
     hero.flipX(false);
+    hero.dir = 1;
     if(!hero.attacking && !hero.hurt && !hero.crouching){
-      hero.move(130, 0)
+      hero.move(SPEED, 0)
     }
   })
   onKeyRelease(['left', 'right'], () => {
@@ -65,26 +144,45 @@ scene('play', () => {
 
   onKeyPress('a', () => {
     hero.attacking = true;
+    swordCollider.attacking = true;
   })
 
   hero.onAnimEnd('attack', () => {
     hero.attacking = false;
+    swordCollider.attacking = false;
+  })
+
+  hero.onCollide("enemy", (e) => {
+    hero.hurt = true;
   })
 
   onKeyPress('space', () => {
     hero.jump(500)
   })
 
+  every("enemy", (e) => {
+    e.onUpdate(() => {
+      if(e.isTouching(get('sword')[0])){
+        if(get('sword')[0].attacking){
+          wait(0.4, () => e.destroy());
+        }
+        // debug.log('touching')
+      }
+    })
+  })
+
   const handleAnims = () => {
     const curAnim = hero.curAnim();
-    if(hero.fall){
+    if(hero.hurt){
+      if(curAnim !== 'hurt'){
+        hero.play('hurt');
+        wait(0.3, () => hero.hurt = false)
+      }
+    }
+    else if(hero.fall){
       if(curAnim !== 'fall'){
         hero.play('fall');
       }
-    // }else if(hero.jumping){
-    //   if(curAnim !== 'jump'){
-    //     hero.play('jump');
-    //   }
     }else if(hero.attacking){
       if(curAnim !== 'attack'){
         hero.play('attack');
@@ -107,12 +205,17 @@ scene('play', () => {
   hero.onUpdate(() => {
     handleAnims();
     // debug.log(hero.onAnimEnd);
-    camPos({x: hero.pos.x, y: -20});
+    camPos(vec2(hero.pos.x, 10));
+    // camPos(hero.pos)
 
     if(hero.isGrounded()){
       hero.fall = false;
     }else {
       hero.fall = true;
+    }
+
+    if(hero.hurt){
+      hero.move(-DISPLACEMENT * hero.dir, 0);
     }
 
     if(hero.crouching){
@@ -121,7 +224,7 @@ scene('play', () => {
       hero.area.height = 30;
     }
 
-    // debug.log(hero.fall)
+    // debug.log(camPos())
   })
 })
 
